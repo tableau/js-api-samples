2af77e36cbd8babec1f87c7a3a55c260cd2c76eb
Michael Kovner <mike.kovner@gmail.com>
2016-01-28 12:12:38 -0800

    Add Vid 03 Code

---
+++ 03 - Embedding and Interactions/jsapi_embed_interactions_beginning.js
@@ -0,0 +1,22 @@
+// Initialize the viz variable 
+
+
+window.onload= function() {
+// When the webpage has loaded, load the viz
+    
+};
+
+// Switch the viz to the sheet specified
+function switchView(sheetName) {
+	
+}
+
+// Filter the specified dimension to the specified value(s)
+function show(filterName, values) {
+    
+}
+
+// Select the marks that have the specified value(s) for the specified dimension
+function selectMarks(filterName, values) {
+    
+}
